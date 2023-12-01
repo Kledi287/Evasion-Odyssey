1366c5124f2c21b3d018d08d0a6f4a754e0ad874
Kledi287 <Kledi.Azzopardi.d56725@mcast.edu.mt>
2023-12-01 19:13:32 +0100

    Update 10
    Added enemy states (idle, attack, chase) now I just need to apply damage taken on hit on the player and hp.

--- Assets/Scripts/States/IdleState.cs
+++ Assets/Scripts/States/IdleState.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class IdleState : StateMachineBehaviour
 {
     float timer;
+    float chaseRange = 8;
+    Transform player;
+
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         timer = 0;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -18,6 +23,13 @@ public class IdleState : StateMachineBehaviour
         if (timer > 5)
         {
             animator.SetBool("isPatrolling", true);
+        }
+
+        float distance = Vector3.Distance(player.position, animator.transform.position);
+
+        if (distance < chaseRange)
+        {
+            animator.SetBool("isChasing", true) ;
         }
     }
 
