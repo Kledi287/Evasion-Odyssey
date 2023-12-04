ca3115f55be52068e606d058d2ca46b867296b02
Kledi287 <Kledi.Azzopardi.d56725@mcast.edu.mt>
2023-12-04 12:02:18 +0100

    Update 13
    Added health bar for player, also when player dies he is unable to move.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public Animator animator;
+    public Slider healthBar;
     public int currentHealth = 100;
     public float normalDamageCooldown = 2f;
     private float deathDamageCooldown = 100f; 
@@ -13,6 +15,7 @@ public class Player : MonoBehaviour
 
     void Start()
     {
+        healthBar.value = currentHealth;
         currentDamageCooldown = normalDamageCooldown;
     }
 
@@ -21,11 +24,17 @@ public class Player : MonoBehaviour
     
     }
 
+    public bool IsAlive()
+    {
+        return currentHealth > 0;
+    }
+
     public void TakeDamage(int damageAmount)
     {
         if (canTakeDamage)
         {
             currentHealth -= damageAmount;
+            healthBar.value -= damageAmount;
 
             if (currentHealth <= 0)
             {
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,16 +13,19 @@ public class PlayerMovement : MonoBehaviour
 
     private Animator animator;
     private CharacterController characterController;
+    private Player player;
     private float ySpeed;
     private float originalStepOffset;
     private float? lastGroundedTime;
     private float? jumpButtonPressedTime;
     private bool isJumping;
     private bool isGrounded;
+   
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GetComponent<Player>();
         animator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
         originalStepOffset = characterController.stepOffset;
@@ -31,84 +34,91 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
-
-        if (Input.GetKey(KeyCode.LeftShift) == false && Input.GetKey(KeyCode.RightShift) == false)
+        if (player != null && player.IsAlive())
         {
-            inputMagnitude /= 2;
-        }
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
 
-        animator.SetFloat("InputMagnitude", inputMagnitude, 0.05f, Time.deltaTime);
+            Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+            float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
 
-        movementDirection = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up) * movementDirection;
-        movementDirection.Normalize();
-
-        ySpeed += Physics.gravity.y * Time.deltaTime;
-
-        if (characterController.isGrounded)
-        {
-            lastGroundedTime = Time.time;
-        }
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            jumpButtonPressedTime = Time.time;
-        }
-
-        if (Time.time - lastGroundedTime <= jumpButtonGracePeriod)
-        {
-            characterController.stepOffset = originalStepOffset;
-            ySpeed = -0.5f;
-            animator.SetBool("isGrounded", true);
-            isGrounded = true;
-            animator.SetBool("isJumping", false);
-            isJumping = false;
-            animator.SetBool("isFalling", false);
-
-            if (Time.time - jumpButtonPressedTime <= jumpButtonGracePeriod)
+            if (Input.GetKey(KeyCode.LeftShift) == false && Input.GetKey(KeyCode.RightShift) == false)
             {
-                ySpeed = jumpSpeed;
-                animator.SetBool("isJumping", true);
-                isJumping = true;
-                jumpButtonPressedTime = null;
-                lastGroundedTime = null;
+                inputMagnitude /= 2;
+            }
+
+            animator.SetFloat("InputMagnitude", inputMagnitude, 0.05f, Time.deltaTime);
+
+            movementDirection = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up) * movementDirection;
+            movementDirection.Normalize();
+
+            ySpeed += Physics.gravity.y * Time.deltaTime;
+
+            if (characterController.isGrounded)
+            {
+                lastGroundedTime = Time.time;
+            }
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                jumpButtonPressedTime = Time.time;
+            }
+
+            if (Time.time - lastGroundedTime <= jumpButtonGracePeriod)
+            {
+                characterController.stepOffset = originalStepOffset;
+                ySpeed = -0.5f;
+                animator.SetBool("isGrounded", true);
+                isGrounded = true;
+                animator.SetBool("isJumping", false);
+                isJumping = false;
+                animator.SetBool("isFalling", false);
+
+                if (Time.time - jumpButtonPressedTime <= jumpButtonGracePeriod)
+                {
+                    ySpeed = jumpSpeed;
+                    animator.SetBool("isJumping", true);
+                    isJumping = true;
+                    jumpButtonPressedTime = null;
+                    lastGroundedTime = null;
+                }
+            }
+            else
+            {
+                characterController.stepOffset = 0;
+                animator.SetBool("isGrounded", false);
+                isGrounded = false;
+
+                if ((isJumping && ySpeed < 0) || ySpeed < -4)
+                {
+                    animator.SetBool("isFalling", true);
+                }
+            }
+
+            if (movementDirection != Vector3.zero)
+            {
+                animator.SetBool("isMoving", true);
+
+                Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+
+            if (isGrounded == false)
+            {
+                Vector3 velocity = movementDirection * inputMagnitude * jumpHorizontalSpeed;
+                velocity.y = ySpeed;
+
+                characterController.Move(velocity * Time.deltaTime);
             }
         }
         else
         {
-            characterController.stepOffset = 0;
-            animator.SetBool("isGrounded", false);
-            isGrounded = false;
-
-            if ((isJumping && ySpeed < 0) || ySpeed < -4)
-            {
-                animator.SetBool("isFalling", true);
-            }
-        }
-
-        if (movementDirection != Vector3.zero)
-        {
-            animator.SetBool("isMoving", true);
-
-            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-        }
-        else
-        {
-            animator.SetBool("isMoving", false);
-        }
-
-        if (isGrounded == false)
-        {
-            Vector3 velocity = movementDirection * inputMagnitude * jumpHorizontalSpeed;
-            velocity.y = ySpeed;
-
-            characterController.Move(velocity * Time.deltaTime);
+            characterController.Move(Vector3.zero);
         }
     }
 
